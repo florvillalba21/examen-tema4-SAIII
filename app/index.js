1ba04d152a70c5378d63e349b863fc18f9a06685
Florcita <florvllb33@gmailcom>
2023-10-09 16:33:35 -0300

    mejorando comentarios

--- app/index.js
+++ app/index.js
@@ -3,13 +3,16 @@ const axios = require("axios");
 const app = express();
 const PORT = 3000;
 
-app.set("view engine", "ejs"); // Configura EJS como el motor de plantillas
+app.set("view engine", "ejs");
 app.use(express.json());
 
+// pagina principal
 app.get("/", async(req, res)=>{
     const images = [];
     res.render("index", {images})
 })
+
+// retorna las imagenes en base a la raza
 app.get("/:raza", async (req, res) => {
   const perroRaza = req.params.raza;
   const urlBase = `https://dog.ceo/api/breed/${perroRaza}/images`;
@@ -18,12 +21,12 @@ app.get("/:raza", async (req, res) => {
     const response = await axios.get(urlBase);
     const images = response.data.message;
     console.log(perroRaza)
+    
     // Renderiza el template 'index.ejs' y envía las imágenes como datos
     res.setHeader("Content-Type", "application/json");
     res.json({ images });
   } catch (error) {
     console.error(error);
-    // Enviar un mensaje de error al cliente si la solicitud a la API falla
     res.status(500).json({ error: "Error al obtener las imágenes de la API" });
   }
 });
